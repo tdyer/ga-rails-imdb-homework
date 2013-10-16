a7f15d5507541181291b7a83968bd9bbb3315e6b
Rahul Horé <hore.rahul@gmail.com>
2013-10-16 19:53:17 -0400

    Generates Movie scaffold and migrates database

---
+++ imdb_app/db/migrate/20131016235022_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.text :description
+      t.date :release_date
+      t.integer :rating
+      t.integer :length
+
+      t.timestamps
+    end
+  end
+end
