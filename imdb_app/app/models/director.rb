edc84d3e1c9cdcac395a1f2a4827f95407ddcc22
Ben Winter <ben.g.winter@gmail.com>
2013-10-16 19:10:38 -0400

    added entry validations

--- imdb_app/app/models/director.rb
+++ imdb_app/app/models/director.rb
@@ -1,11 +1,5 @@
 class Director < ActiveRecord::Base
   has_many :movies
-  # validate :name, presence: true, length: { in: 4..30}
-  # validate :dob, presence: true
-  # validate :dob_cannot_be_in_the_past,
-  # def dob_cannot_be_in_the_past
-  #   if dob.present? && dob < Date.today
-  #     errors.add(:dob, "can't be in the past")
-  #   end
-  # end
+  validates :name, presence: true, length: { in: 4..30}
+  validates :dob, presence: true
 end
--- imdb_app/app/models/movie.rb
+++ imdb_app/app/models/movie.rb
@@ -1,8 +1,8 @@
 class Movie < ActiveRecord::Base
   belongs_to :director
-  # validates :name, presence: true, length: { in: 5..20}
-  # validates :release_date, presence: true
-  # validates :length, numericality: { only_integer: true }, numericality: { less_than_or_equal_to: 300}
-  # validates :rating, presence: true
-  # validates_numericality_of :rating, on: :create
+  validates :name, presence: true, length: { in: 5..20}
+  validates :release_date, presence: true
+  validates :length, numericality: { only_integer: true }, numericality: { less_than_or_equal_to: 300}
+  validates :rating, presence: true, numericality: { only_float: true }
+  validates_numericality_of :rating, on: :create
 end
