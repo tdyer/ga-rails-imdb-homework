a670cac89c56d5b6198208895f31b590f084e052
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-16 18:31:54 -0400

    starting steps to create my own Imdb

---
+++ ImdbCopy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImdbCopy::Application.config.secret_key_base = '0996e69b7da886cbd74589a21ac876d1baa43984d86390a97145fb68f387b72dc71a996e43a99e7145a7c41067932e55ea2f9789c45cb8bd08d75cc61d283905'
---
+++ ImdbCopy/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImdbCopy::Application.config.session_store :cookie_store, key: '_ImdbCopy_session'
