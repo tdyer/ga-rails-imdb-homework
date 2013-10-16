5272b154fa855738203294768af5e364ceb30966
Ben Winter <ben.g.winter@gmail.com>
2013-10-16 18:34:35 -0400

    solved data base seed issues and seeded

--- imdb_app/app/controllers/movies_controller.rb
+++ imdb_app/app/controllers/movies_controller.rb
@@ -69,6 +69,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:name, :release_date, :rating, :length, :description)
+      params.require(:movie).permit(:name, :release_date, :rating, :length, :description, :director_id)
     end
 end
