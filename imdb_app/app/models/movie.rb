58e9e97a47d5b9a7b5d0f82dd4c9ef8c629594b1
Abby Howell <abby.g.howell@gmail.com>
2013-10-16 22:39:50 -0400

    actually seeded database

--- imdb_app/app/models/movie.rb
+++ imdb_app/app/models/movie.rb
@@ -2,5 +2,6 @@ class Movie < ActiveRecord::Base
   belongs_to :director
   validates :name, presence: true, length: { minimum: 5, maximum: 20}
   validates :length, length: {maximum: 300}
-  validates :date, :rating, :director, presence: true
+  validates :date, presence: true
+  validates :rating, presence: true
 end
