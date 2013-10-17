7cc23261f3644341fd010ec9c64c2ace83e297df
Cori Allen <coriallen@gmail.com>
2013-10-16 21:37:49 -0400

    Define movie and director controller actions.

--- IMDB_Clone/app/controllers/director_controller.rb
+++ IMDB_Clone/app/controllers/director_controller.rb
@@ -1,22 +1,48 @@
 class DirectorController < ApplicationController
+  before_action :set_director, only: [:edit, :show, :update, :destroy]
   def new
   end
 
   def create
+    @director = Director.new(movie_params)
+      if @director.save
+       redirect_to @director
+      else
+        render action: 'new'
+      end
   end
 
   def index
+    @movies = Director.all
   end
 
   def show
+    @director = Director.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @director.update(director_params)
+      redirect_to @director
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @director.destroy
+    redirect_to directors_url
+  end
+
+  private
+
+  def set_director
+    @director = Director.find(params[:id])
+  end
+
+  def director_params
+    params.require(:director), permit(:name, :description, :birthday)
   end
 end
--- IMDB_Clone/app/controllers/movie_controller.rb
+++ IMDB_Clone/app/controllers/movie_controller.rb
@@ -1,22 +1,48 @@
 class MovieController < ApplicationController
+  before_action :set_movie, only: [:edit, :show, :update, :destroy]
   def new
   end
 
   def create
+    @movie = Movie.new(movie_params)
+      if @movie.save
+       redirect_to @movie
+      else
+        render action: 'new'
+      end
   end
 
   def index
+    @movies = Movie.all
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @movie.update(movie_params)
+      redirect_to @movies
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @movie.destroy
+    redirect_to movies_url
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie), permit(:name, :description, :release_date, :rating, :length)
   end
 end
