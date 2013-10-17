bc97b19ee3731ebdbdde49a6b27e3f5f1a988efc
Michael Finneran <mrfinneran@gmail.com>
2013-10-17 11:56:36 -0400

    created actor model

--- IMDB/db/schema.rb
+++ IMDB/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017010523) do
+ActiveRecord::Schema.define(version: 20131017155258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "directors", force: true do |t|
     t.string   "name"
