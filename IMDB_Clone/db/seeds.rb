d0bbd7eb3482636a40552bbc8b8e3eb5025b23be
Cori Allen <coriallen@gmail.com>
2013-10-16 21:02:13 -0400

    Seed directors, change rating to decimal.

--- IMDB_Clone/db/seeds.rb
+++ IMDB_Clone/db/seeds.rb
@@ -1,4 +1,6 @@
-Director.create(name: 'Sofia Coppola', description: 'Oscar-winner and renowned director of moody, visually opulent, candy-color-bright films with discordant music and introspective characters.', birthday: 1971-09-14)
-Director.create(name: 'Nora Ephron', description: '', birthday: yyyy-mm-dd)
-Director.create(name: 'Terrence Malik', description: 'Reclusive and exacting master of emotion-evoking imagery, releases films only one or twice a decade, resides in Austin, Texas, has not yet won an Oscar.', birthday: yyyy-mm-dd)
-Director.create(name: 'David Fincher', description: '', birthday: yyyy-mm-dd)
+Director.create(name: 'Sofia Coppola', description: 'Oscar-winner and renowned director of moody, visually opulent, candy-color-bright films with discordant music and introspective characters.') # birthday: 14/9/1971)
+Director.create(name: 'Nora Ephron', description: 'Technically a writer, three time oscar nominee and creator of such classics as youve got mail, sleepless in seattle, and when harry met sally, Nora Ephron is a legend.') # birthday: 19/9/1941)
+Director.create(name: 'Terrence Malik', description: 'Reclusive and exacting master of emotion-evoking imagery, releases films only one or twice a decade, resides in Austin, Texas, has not yet won an Oscar.') # birthday: 30/11/1943)
+Director.create(name: 'David Fincher', description: 'Dark and unforgiving movies, as well as sentimental, political, and tech-culture films, have made Fincher a director whose projects you go see no matter what.') # birthday: 28/8/1962)
+
+Movie.create(name: '', description: ' ', rating: '')
