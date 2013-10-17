ee467a50beabfb1b2c6d490c39303da3b46eeb83
Michael Finneran <mrfinneran@gmail.com>
2013-10-16 22:59:21 -0400

    in movie show can add a director if non was assigned or in movie update can change director, added a field in director show to add a movie from the list that was not assigned or steal from another director but could not complete task.

--- IMDB/app/models/movie.rb
+++ IMDB/app/models/movie.rb
@@ -4,7 +4,7 @@ class Movie < ActiveRecord::Base
   validates :release_date, presence: true
   validates :rating, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10} 
   validates_length_of :name, minimum: 5, maximum: 20, allow_blank: false
-  validates :director_id, presence: true
+  # validates :director_id, presence: true
   validates :movie_length, numericality: {only_integer: true, less_than_or_equal_to: 300}
 
 end
