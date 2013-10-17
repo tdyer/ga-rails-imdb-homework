9c29cebc01435919faa87fbec7f66ca5d765064b
Ben Winter <ben.g.winter@gmail.com>
2013-10-17 11:56:32 -0400

    created actor model

--- imdb_app/db/migrate/20131017155313_create_actors.rb
+++ imdb_app/db/migrate/20131017155313_create_actors.rb
@@ -1,8 +1,8 @@
 class CreateActors < ActiveRecord::Migration
   def change
     create_table :actors do |t|
-      t.string :name
-
+      t.string :first_name
+      t.string :last_name
       t.timestamps
     end
   end
