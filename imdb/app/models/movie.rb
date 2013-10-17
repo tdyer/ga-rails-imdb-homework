9a3511365cf1920f71d5b954cfe26cd5f14ae7f5
Blake Ruddock <ruddock.blake@gmail.com>
2013-10-16 20:11:35 -0400

    Added validation for movies form.

--- imdb/app/models/movie.rb
+++ imdb/app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
 	belongs_to :director
+	validates :name, presence: true, length: {minimum: 5}
+	validates :name, length: {maximum: 20}
+	validates :release, presence: true
+	validates :length, numericality: {less_than_or_equal_to: 300}
 end
