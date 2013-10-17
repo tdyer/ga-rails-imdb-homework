e8f69918823554cba383a88a77c0c4bcb1686da3
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-16 21:43:19 -0400

    nuked it and starting over

--- ImdbCopy/app/views/directors/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @director, :nam, :birth_date, :death_date, :location, :bio, :created_at, :updated_at
--- ImdbCopy/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-ImdbCopy::Application.config.session_store :cookie_store, key: '_ImdbCopy_session'
